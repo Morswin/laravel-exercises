c5d2233675f7ce740dbdf96fe8101591074d3346
Morswin <41349347+Morswin@users.noreply.github.com>
2024-01-07 21:42:05 +0100

    type crud function

--- my-app/app/Http/Controllers/PeopleController.php
+++ my-app/app/Http/Controllers/PeopleController.php
@@ -29,7 +29,7 @@ class PeopleController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(string $id): JsonResponse
     {
         return response()->json(People::find($id));
     }
